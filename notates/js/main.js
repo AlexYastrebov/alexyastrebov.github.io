7b36acded66909a9fc901fc804e0d9409aa58804
AlexYastrebov <lexus-lexus86@ukr.net>
2022-10-23 20:28:53 +0200

    changed 23.10

--- notates/js/main.js
+++ notates/js/main.js
@@ -14,6 +14,8 @@ class Notate {
         let newImg = document.createElement('img');
         newImg.classList.add('notate__img');
         newImg.src = this.img;
+        newImg.src ='/images/emojione_bird-logo.svg';
+        // как сделать фото как лого?
 
         let newTitle = document.createElement('h2');
         newTitle.classList.add('notate__title');
@@ -26,6 +28,10 @@ class Notate {
         let newBtnEdit = document.createElement('button');
         newBtnEdit.classList.add('notate__btn');
         newBtnEdit.textContent = 'Edit';
+        newBtnEdit.dataset.edit = 'false';
+
+        newBtnEdit.addEventListener('click', (e) => {this.toggleEdit(e)})
+
 
         let newBtnDelete = document.createElement('button');
         newBtnDelete.classList.add('notate__btn');
@@ -40,14 +46,45 @@ class Notate {
         newNotate.appendChild(newBtnDelete)
 
         notates.parent.appendChild(newNotate);
+
+
     }
     save(){
         this.create(form.title.value, form.text.value, form.img.value)
     }
-    edit(){
+    toggleEdit(e) {
+        this.e = e;
+        if (this.e.target.dataset.edit == 'false') {
+            this.edit(e);
 
+        } else {
+            this.saveEdit(e);
+        }
+    }
+    saveEdit(btn) {
+        this.btn = btn.target;
+        this.btn.style.background = 'gray';
+        this.btn.textContent = 'edit';
+    }
+    edit(e){
+        this.e = e;
+        this.e.target.dataset.edit = 'true';
+        this.editTitle = (this.e.target.parentElement).getElementsByClassName('notate__title')[0];
+        this.editText = (this.e.target.parentElement).getElementsByClassName('notate__text')[0];
+        
+        this.editTitle.contentEditable = "true";
+        this.editText.contentEditable = "true";
+        this.editTitle.focus();
+        this.editTitle.select();
+
+        this.changeBtnEdit(this.e.target);
     }
     delete(){
+    }
+    changeBtnEdit(btn) {
+        this.btn = btn;
+        this.btn.style.background = 'green';
+        this.btn.textContent = 'save';
 
     }
 }
