3c92f2770ab7db8336a287fd799fbdfd7c808505
AlexYastrebov <lexus-lexus86@ukr.net>
2022-11-02 08:14:29 +0100

    new files

--- quetions/js/database.js
+++ quetions/js/database.js
@@ -7,138 +7,138 @@ const DB = {
         },
         {
             quiz: 'HTML состоит только из Тегов?',
-            answers: ['yes', 'no'],
-            rightAnswer: 'yes'
+            answers: ['да', 'нет'],
+            rightAnswer: 'да'
         },
         {
             quiz: 'Существуют ли непарные Теги?',
-            answers: ['yes', 'no'],
-            rightAnswer: 'yes'
+            answers: ['да', 'нет'],
+            rightAnswer: 'да'
         },
         {
             quiz: 'Можно ли использовать несколько видов списков при создании веб-страницы?',
-            answers: ['yes', 'no'],
-            rightAnswer: 'yes'
+            answers: ['да', 'нет'],
+            rightAnswer: 'да'
         },
         {
             quiz: 'Можно ли писать комментарии в HTML-коде?',
-            answers: ['yes', 'no'],
-            rightAnswer: 'yes'
+            answers: ['да', 'нет'],
+            rightAnswer: 'да'
         },
         {
             quiz: 'Можно ли присваивать значения не всем атрибутам тега?',
-            answers: ['yes', 'no'],
-            rightAnswer: 'yes'
+            answers: ['да', 'нет'],
+            rightAnswer: 'да'
         },
         {
             quiz: 'Можно ли в коде вставить ссылку на другую страницу?',
-            answers: ['yes', 'no'],
-            rightAnswer: 'yes'
+            answers: ['да', 'нет'],
+            rightAnswer: 'да'
         },
         {
             quiz: 'Нужно ли пользоваться отступами во время написания кода?',
-            answers: ['yes', 'no'],
-            rightAnswer: 'yes'
+            answers: ['да', 'нет'],
+            rightAnswer: 'да'
         },
         {
             quiz: 'Обязательно ли использовать альтернативный текст (атрибут alt тега img) для изображений?',
-            answers: ['yes', 'no'],
-            rightAnswer: 'yes'
+            answers: ['да', 'нет'],
+            rightAnswer: 'да'
         },
         {
             quiz: 'Гиперссылка может быть только текстовой?',
-            answers: ['yes', 'no'],
-            rightAnswer: 'no'
+            answers: ['да', 'нет'],
+            rightAnswer: 'нет'
         }
         ],
         
     css: [{
             quiz: 'Свойство стиля position, block?',
-            answers: ['yes', 'no'],
-            rightAnswer: 'no'
+            answers: ['да', 'нет'],
+            rightAnswer: 'нет'
         },
         {
             quiz: 'Стиль padding отвечает за внешний отступ от элемента?',
-            answers: ['yes', 'no'],
-            rightAnswer: 'no'
+            answers: ['да', 'нет'],
+            rightAnswer: 'нет'
         },
         {
             quiz: 'Стиль opacity отвечает за прозрачность?',
-            answers: ['yes', 'no'],
-            rightAnswer: 'yes'
+            answers: ['да', 'нет'],
+            rightAnswer: 'да'
         },{
             quiz: 'CSS расшифровывается как Common Style Sheets?',
-            answers: ['yes', 'no'],
-            rightAnswer: 'no'
+            answers: ['да', 'нет'],
+            rightAnswer: 'нет'
         },{
             quiz: 'Синтаксически корректное CSS правило: div [font-size:2em]?',
-            answers: ['yes', 'no'],
-            rightAnswer: 'no'
+            answers: ['да', 'нет'],
+            rightAnswer: 'нет'
         },{
             quiz: 'Свойство позволяющее устанавливать размер шрифта называется font-weight?',
-            answers: ['yes', 'no'],
-            rightAnswer: 'no'
+            answers: ['да', 'нет'],
+            rightAnswer: 'нет'
         },{
             quiz: 'Свойства border-radius и border это одно и тоже?',
-            answers: ['yes', 'no'],
-            rightAnswer: 'yes'
+            answers: ['да', 'нет'],
+            rightAnswer: 'нет'
         },{
             quiz: 'Можно ли использовать position absolute без position relative?',
-            answers: ['yes', 'no'],
-            rightAnswer: 'no'
+            answers: ['да', 'нет'],
+            rightAnswer: 'нет'
         },{
             quiz: 'Можно ли в свойстве background задать несколько значений?',
-            answers: ['yes', 'no'],
-            rightAnswer: 'yes'
+            answers: ['да', 'нет'],
+            rightAnswer: 'да'
         },{
             quiz: 'Файл css в проекте обязательно ли подключать последним?',
-            answers: ['yes', 'no'],
-            rightAnswer: 'no'
+            answers: ['да', 'нет'],
+            rightAnswer: 'нет'
         }
         ],
 
     js: [{
-            quiz: 'HTML расшифровывается как HyperText Marketing Language?',
-            answers: ['yes', 'no'],
-            rightAnswer: 'yes'
+            quiz: 'JavaScript используется в backend?',
+            answers: ['да', 'нет'],
+            rightAnswer: 'нет'
         },
         {
-            quiz: 'HTML расшифровывается как HyperText Marketing Language?',
-            answers: ['yes', 'no'],
-            rightAnswer: 'yes'
+            quiz: 'JavaScript является объектно-ориентированным языком программирования?',
+            answers: ['да', 'нет'],
+            rightAnswer: 'да'
         },
         {
-            quiz: 'HTML расшифровывается как HyperText Marketing Language?',
-            answers: ['yes', 'no'],
-            rightAnswer: 'yes'
+            quiz: 'Могут ли названия переменных начинаться с цифры,?',
+            answers: ['да', 'нет'],
+            rightAnswer: 'нет'
         },{
-            quiz: 'HTML расшифровывается как HyperText Marketing Language?',
-            answers: ['yes', 'no'],
-            rightAnswer: 'yes'
+            quiz: 'Относится ли Объектная модель документа к JavaScript?',
+            answers: ['да', 'нет'],
+            rightAnswer: 'да'
         },{
-            quiz: 'HTML расшифровывается как HyperText Marketing Language?',
-            answers: ['yes', 'no'],
-            rightAnswer: 'yes'
+            quiz: 'Для добавления JavaScript-кода на страницу можно использовать теги <js></js>?',
+            answers: ['да', 'нет'],
+            rightAnswer: 'нет'
         },{
-            quiz: 'HTML расшифровывается как HyperText Marketing Language?',
-            answers: ['yes', 'no'],
-            rightAnswer: 'yes'
+            quiz: 'Контейнеров <script> в одном документе может быть сколько угодно?',
+            answers: ['да', 'нет'],
+            rightAnswer: 'да'
         },{
-            quiz: 'HTML расшифровывается как HyperText Marketing Language?',
-            answers: ['yes', 'no'],
-            rightAnswer: 'yes'
+            quiz: 'Будет ли работать проект, если в нём не будет переменной "const"?',
+            answers: ['да', 'нет'],
+            rightAnswer: 'да'
         },{
-            quiz: 'HTML расшифровывается как HyperText Marketing Language?',
-            answers: ['yes', 'no'],
-            rightAnswer: 'yes'
+            quiz: 'JavaScript рекомендуют подключать к проекту отдельным файлом??',
+            answers: ['да', 'нет'],
+            rightAnswer: 'да'
         },{
-            quiz: 'HTML расшифровывается как HyperText Marketing Language?',
-            answers: ['yes', 'no'],
-            rightAnswer: 'yes'
+            quiz: 'Отличается ли чем-то Var от Let?',
+            answers: ['да', 'нет'],
+            rightAnswer: 'да'
         },{
-            quiz: 'HTML расшифровывается как HyperText Marketing Language?',
-            answers: ['yes', 'no'],
-            rightAnswer: 'yes'
+            quiz: 'При написании кода в JavaScript можно ли использовать атрибут "src"?',
+            answers: ['да', 'нет'],
+            rightAnswer: 'да'
         }
         ],
     }
